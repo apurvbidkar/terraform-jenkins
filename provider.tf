1ec48b305b69cdba3bfdd892a27edf92634bdc62
Apurv Vinay Bidkar <97932925+apurvbidkar@users.noreply.github.com>
2022-09-08 19:13:39 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAV4SSGKL6D4OIN6UN"
-  secret_key = "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"
+  access_key = "AKIAV4SSGKL6FXTZXMEH"
+  secret_key = "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"
 }
-
